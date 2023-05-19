88ecd95492adf96f593f166cb3cf8f5a1c7d1959
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-05-19 11:46:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// create p tag
+function Timer() {
+	var currentTime = new Date();
+	// format the date and time
+	var date = currentTime.toLocaleDateString();
+	var time = currentTime.toLocaleTimeString();
+
+	// create p tag 
+	var para = document.createElement("p");
+    para.id = "timer";
+	// add date and time inside para
+	para.textContent = date+ ", " +time;
+	document.body.appendChild(para);
+}
+
+Timer();
+//setInterval(Timer,1000);
